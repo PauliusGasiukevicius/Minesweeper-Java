2a67b40ec29117d62d6a91f549fff9de327cc74a
Paulius Gasiukevičius <paulius.gasiukevicius@gmail.com>
2018-11-26 13:50:03 +0200

    Implemented some code that prevent player from losing at first turn

--- src/minesweeper/GameLogic.java
+++ src/minesweeper/GameLogic.java
@@ -4,12 +4,14 @@ public class GameLogic {
 
     private GameBoard board;
     private boolean gameOver;
+    private boolean firstClick;
     private int cellsLeftToOpen;
 
     public GameLogic(GameBoard board) {
         this.board = board;
         this.gameOver = false;
         this.cellsLeftToOpen = board.getHeight() * board.getWidth() - board.getMineCount();
+        this.firstClick = true;
     }
 
     public boolean isGameOver() {
@@ -25,23 +27,31 @@ public class GameLogic {
         cellsLeftToOpen--;
     }
 
-    public boolean canOpenNeighbour(int r, int c) {
-        return board.isValidCell(r, c) && board.cellNeighbouringMinesCount(r, c) == 0 && !board.isCellMined(r, c);
-    }
-
     public void openZeroCells(int r, int c) {
         if (!board.isValidCell(r, c) || board.getCellState(r, c) == CELL.OPEN) {
             return;
         }
         openCell(r, c);
-        boolean openAll = board.cellNeighbouringMinesCount(r, c) == 0;
+        boolean openNeighbours = board.cellNeighbouringMinesCount(r, c) == 0;
 
         int[] dr = {-1, 0, 1, 0, -1, -1, 1, 1};
         int[] dc = {0, -1, 0, 1, -1, 1, -1, 1};
 
         for (int k = 0; k < dr.length; k++) {
-            if (openAll || canOpenNeighbour(r + dr[k], c + dc[k])) {
+            if (openNeighbours) {
                 openZeroCells(r + dr[k], c + dc[k]);
+            }
+        }
+    }
+
+    private void swapCellsIfFirstTurn(int r, int c) {
+        for (int i = 0; i < board.getHeight(); i++) {
+            for (int j = 0; j < board.getWidth(); j++) {
+                if (board.getCellState(i, j) == CELL.EMPTY) {
+                    board.setCellState(i, j, board.getCellState(r, c));
+                    board.setCellState(r, c, CELL.EMPTY);
+                    return;
+                }
             }
         }
     }
@@ -50,7 +60,16 @@ public class GameLogic {
         if (!board.isValidCell(r, c)) {
             return;
         }
+
         CELL current = board.getCellState(r, c);
+
+        if (firstClick) {
+            firstClick = false;
+            if (current == CELL.MINE || current == CELL.FLAGM || current == CELL.EXPLOSION) {
+                swapCellsIfFirstTurn(r, c);
+                current = board.getCellState(r, c);
+            }
+        }
 
         switch (current) {
             case FLAGM:
